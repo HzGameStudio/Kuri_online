f47599a00dc47eee66776fe49766d138b303124f
flowwothchicken <oleksiivinichenko@gmail.com>
2023-07-27 10:37:33 +0200

    Im gay + added photon

---
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField CreateInput;
+    public TMP_InputField JoinInput;
+
+   public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(CreateInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(JoinInput.text);
+    }
+
+    public void OpenK()
+    {
+        TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("SampleScene");
+    }
+}
