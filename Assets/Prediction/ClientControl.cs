c0fc1e97f2eb93fbb2415f40a638ce385c58e87f
Ya2403 <120574054+Ya2403@users.noreply.github.com>
2023-09-16 00:48:25 +0300

    new scene added, please test with relay

---
+++ Assets/Prediction/ClientControl.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.Scripting.APIUpdating;
+
+public class ClientControlwww : NetworkBehaviour
+{
+
+    [SerializeField] GameObject Camera;
+
+    private Rigidbody2D rb;
+    public override void OnNetworkSpawn()
+    {
+        if(!IsOwner)
+        {
+            Camera.SetActive(false);
+        }
+        base.OnNetworkSpawn();
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        if(IsOwner)
+        {
+            transform.position =new Vector3(transform.position.x + Random.Range(5, 10), transform.position.y, transform.position.z);
+        }
+    }
+
+    void Update()
+    {
+        
+        if(IsOwner)
+        {
+            Move(GetInput());
+        }
+    }
+
+    void Move(Vector2 input)
+    {
+        float acseleration = 9;
+        rb.AddForce(input * 5f);
+    }
+
+    Vector2 GetInput()
+    {
+        Vector2 input = new();
+        if(Input.GetKey(KeyCode.W))
+        {
+            input.y = 1;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            input.y = -1;
+        }
+        input.x = 0;
+        return input;
+    }
+}
