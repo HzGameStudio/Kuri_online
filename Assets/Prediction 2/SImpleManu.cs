02251c375bea3b2d8911c913055f11383043349f
Antonov Ivan <vanoantman@gmail.com>
2023-10-08 22:51:06 +0200

    Add new implementation of netcode

---
+++ Assets/Prediction 2/SImpleManu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class SImpleManu: MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        }
+
+        GUILayout.EndArea();
+    }
+
+}
---
+++ Assets/Prediction 2/TestClientProdictionFromBegining.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using TMPro;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Windows;
+
+public class TestClientProdictionFromBegining : NetworkBehaviour
+{
+    Rigidbody2D rb;
+
+    bool requestMovement = false;
+
+    [SerializeField] GameObject MainCamera;
+
+    private int tick = 0;
+    private float tickRate = 1f / 60f;
+    private float tickDeltaTime = 0f;
+
+    private const int buffer = 1024;
+
+    private HandleState.InputState[] inputStates = new HandleState.InputState[buffer];
+
+    private HandleState.TransformStateRW[] transformStates = new HandleState.TransformStateRW[buffer];
+
+    public NetworkVariable<HandleState.TransformStateRW> currentServerTransformState = new NetworkVariable<HandleState.TransformStateRW>();
+
+    public HandleState.TransformStateRW previousTransformState;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            MainCamera.SetActive(true);
+            return;
+        }
+
+        HandleState.TransformStateRW transformState = new()
+        {
+            tick = tick,
+            finalPos = transform.position,
+            isMoving = true
+        };
+
+        currentServerTransformState.Value = transformState;
+    }
+
+    private void OnEnable()
+    {
+        currentServerTransformState.OnValueChanged += OnServerStateChanged;
+    }
+
+    private void ProcessLocalPlayerMovement(bool moveInput_input)
+    {
+        tickDeltaTime += Time.deltaTime;
+
+        if (tickDeltaTime > tickRate)
+        {
+            int bufferIndex = tick % buffer;
+
+            MovePlayerWithServerTickServerRPC(tick, moveInput_input);
+            Move(moveInput_input);
+
+            HandleState.InputState inputState = new()
+            {
+                tick = tick,
+                moveInput = moveInput_input
+            };
+
+            HandleState.TransformStateRW transformState = new()
+            {
+                tick = tick,
+                finalPos = transform.position,
+                isMoving = true
+            };
+
+            inputStates[bufferIndex] = inputState;
+            transformStates[bufferIndex] = transformState;
+
+            tickDeltaTime -= tickRate;
+
+            if (tick >= buffer)
+                tick = 0;
+            else
+                tick++;
+        }
+    }
+
+    [ServerRpc]
+    private void MovePlayerWithServerTickServerRPC(int tick, bool moveInput_input)
+    {
+        if (!IsOwner)
+            Move(moveInput_input);
+
+        HandleState.TransformStateRW transformState = new()
+        {
+            tick = tick,
+            finalPos = transform.position,
+            isMoving = moveInput_input
+        };
+
+        //Debug.Log(transformState.finalPos);
+
+        previousTransformState = currentServerTransformState.Value;
+        currentServerTransformState.Value = transformState;
+    }
+
+    public void SimulateOtherPlayers()
+    {
+        tickDeltaTime += Time.deltaTime;
+
+        if (tickDeltaTime > tickRate)
+        {
+            //Debug.Log(currentServerTransformState.Value.finalPos);
+            //Debug.Log(OwnerClientId);
+            if (currentServerTransformState.Value.isMoving)
+                transform.position = currentServerTransformState.Value.finalPos;
+
+            tickDeltaTime -= tickRate;
+
+            if (tick == buffer)
+                tick = 0;
+            else
+                tick++;
+        }
+    }
+
+    private void Update()
+    {
+        if(IsOwner)
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.W))
+            {
+                requestMovement = true;
+            }
+        }
+
+        if (IsClient && IsLocalPlayer)
+        {
+            //Debug.Log(OwnerClientId);
+            ProcessLocalPlayerMovement(requestMovement);
+        }
+        else
+        {
+            SimulateOtherPlayers();
+        }
+    }
+
+    void Move(bool flip)
+    {
+        if(flip)
+        {
+            rb.gravityScale = rb.gravityScale * (-1);
+            requestMovement = false;
+        }
+    }
+
+    private void OnServerStateChanged(HandleState.TransformStateRW previousValue, HandleState.TransformStateRW newValue)
+    {
+        //Debug.Log("onvaluechanged" + OwnerClientId.ToString());
+        //if (OwnerClientId == 2)
+           // Debug.Log(previousValue.finalPos.ToString() + ' ' +  newValue.finalPos.ToString());
+        previousTransformState = previousValue;
+    }
+}
