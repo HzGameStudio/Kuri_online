e38e49502dfc468cec0dba5b50f436e8db12ff28
Ya2403 <kondrachuk06@gmail.com>
2023-12-25 00:21:39 +0100

    christmas update, scaling and ofset of grass fixed

--- Assets/Scripts/texureScaleScript.cs
+++ Assets/Scripts/texureScaleScript.cs
@@ -5,14 +5,22 @@ using UnityEngine.U2D;
 
 public class texureScaleScript : MonoBehaviour
 {
-    public Vector3 initialScale;
+    public Vector3 initialGroudScale = new Vector3(1,1,1);
+    public Vector3 initialGrassScale;
+
+    public float deltaForTopGrassBorder;
     public Vector2 initialSize;
     public Transform parentobject;
     public Transform grassTransform;
+    
+  
+    
     void Start()
     {
-        grassTransform.localScale = new Vector3(initialScale.x / parentobject.localScale.x, initialScale.y / parentobject.localScale.y, grassTransform.localScale.z);
+        grassTransform.localScale = new Vector3(initialGrassScale.x / parentobject.localScale.x, initialGrassScale.y / parentobject.localScale.y, grassTransform.localScale.z);
         grassTransform.GetComponent<SpriteRenderer>().size = new Vector2(initialSize.x * parentobject.localScale.x, grassTransform.GetComponent<SpriteRenderer>().size.y);
-        //grassTransform.localPosition = new Vector3(grassTransform.localPosition.x, (parentobject.localScale.y/2 - initialScale.y/2)/parentobject.localScale.y, grassTransform.localPosition.z);
+        
+        grassTransform.position = new Vector3(grassTransform.position.x, parentobject.position.y + initialGroudScale.y* parentobject.localScale.y/2+ deltaForTopGrassBorder, grassTransform.position.z);
+
     }
 }
